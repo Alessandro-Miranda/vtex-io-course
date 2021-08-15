9e49a1492ebed24e557de37f2bcbce29d7216e84
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-08-15 11:40:08 -0300

    Criado o exemplo de conexão a um serviço

--- admin-course/react/adminExample.tsx
+++ admin-course/react/adminExample.tsx
@@ -1,17 +1,23 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 // @ts-ignore
-import { Layout, PageBlock } from 'vtex.styleguide'
+import { Layout, PageBlock } from 'vtex.styleguide';
+import { useQuery } from 'react-apollo';
+
+import helloworld from './graphql/helloworld.graphql';
 
 const AdminExample: FC = () => {
+  const { data } = useQuery(helloworld);
+  
   return (
     <Layout>
       <PageBlock variation="full" title="Título" subTitle="Alguma explicação">
         <h1>Hello, world!!</h1>
+        <p>{data?.helloworld}</p>
       </PageBlock>
     </Layout>
   )
 }
 
-export default AdminExample
+export default AdminExample;
